0d2daa921441cd5f25af67c3b23edab7ae064bf6
SubiGKrishnan <36354686+SubiGKrishnan@users.noreply.github.com>
2018-03-04 12:16:08 +0530

    Update subi.groovy

--- subi.groovy
+++ subi.groovy
@@ -1,3 +1,12 @@
 pipelineJob("job2")
 pipelineJob("job3")
 pipelineJob("job4")
+{
+definition {
+        cps {
+            script(readFileFromWorkspace('Jenkinsfile'))
+            sandbox()
+        }
+    }
+
+}
